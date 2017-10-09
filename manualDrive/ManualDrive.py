6c8e5b6a8237c2dbb726cb302d3dbfb85bbafb56
Rasmus Soome <rsoome16@gmail.com>
2017-10-08 19:59:17 -0700

    updated manula drive module 2.1

--- manualDrive/ManualDrive.py
+++ manualDrive/ManualDrive.py
@@ -19,13 +19,13 @@ class ManualDrive:
 
             keyStroke = screen.getch()
 
-            if keyStroke == ord('w'):
+            if keyStroke == ord('1'):
                 self.move.drive(self.driveSpeed, -60)
 
             if keyStroke == ord('2'):
                 self.move.drive(self.driveSpeed, 0)
 
-            if keyStroke == ord('1'):
+            if keyStroke == ord('s'):
                 self.move.drive(self.driveSpeed, 120)
 
             if keyStroke == ord('s'):
