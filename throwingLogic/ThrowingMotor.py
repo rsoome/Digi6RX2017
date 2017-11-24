ebc3b8aa8a6e317c105c14d4a41741a8a67c1bda
Rasmus Soome <rsoome16@gmail.com>
2017-11-24 08:02:29 -0800

    Configuring throwing motor

--- throwingLogic/ThrowingMotor.py
+++ throwingLogic/ThrowingMotor.py
@@ -5,7 +5,7 @@ class ThrowingMotor:
         self.MIN_SPEED = 1300
         self.MAX_SPEED = 2150
         self.MID_SPEED = (self.MIN_SPEED + self.MAX_SPEED) // 2
-        self.STOP_SPEED = 1000
+        self.STOP_SPEED = 500
 
     def setSpeed(self, speed):
         self.mb.setThrowerSpeed(min(max(speed, self.MIN_SPEED), self.MAX_SPEED))
