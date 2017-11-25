c9a5345c8582513bbd81ee7fdeaee9fab1500ff6
Rasmus Soome <rsoome16@gmail.com>
2017-11-24 20:16:06 -0800

    Changed grabber throw position

--- throwingLogic/Grabber.py
+++ throwingLogic/Grabber.py
@@ -5,7 +5,7 @@ class Grabber:
         self.MAX_POSITION = 2350
         self.MIN_POSITION = 550
         self.CARRY_POSITION = 1950
-        self.THROW_POSITION = self.MIN_POSITION
+        self.THROW_POSITION = 800
         self.OPEN_POSITION = self.MAX_POSITION - 100
 
     def setPosition(self, position):
