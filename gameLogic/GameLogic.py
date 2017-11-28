193ec679e9c4366c7399eb7e7212a473a2ba7189
Rasmus Soome <rsoome16@gmail.com>
2017-11-27 21:14:57 -0800

    Updating game logic

--- gameLogic/GameLogic.py
+++ gameLogic/GameLogic.py
@@ -337,7 +337,7 @@ class GameLogic:
 
     def throwBall(self,distance):
 
-        for i in range(5):
+        for i in range(8):
             if not self.checkHorizontalAlginment(self.basket) or not self.checkVerticalAlignment(self.basket, self.basketStopBound):
                 return False
             time.sleep(0.033)
