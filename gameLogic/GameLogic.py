c7941f6b6d8acc73ea005329fa08820b41f2d426
Rasmus Soome <rsoome16@gmail.com>
2017-12-04 13:44:29 -0800

    Updated game logic

--- gameLogic/GameLogic.py
+++ gameLogic/GameLogic.py
@@ -343,15 +343,15 @@ class GameLogic:
         self.move.stop()
         self.mb.sendValues(wait=True)
 
-        not_alligned = 0
+        '''not_alligned = 0
         for i in range(6):
             if not self.checkHorizontalAlginment(self.basket) or not self.checkVerticalAlignment(self.basket, self.basketStopBound):
                 not_alligned += 1
             time.sleep(0.033)
 
         if not_alligned >= 3:
-            return False
-        
+            return False'''
+
         self.mb.disableFailSafe()
         self.thrower.startMotor()
         self.mb.sendValues(wait = True)
