7347f05decd071d933bddd6fa57c29c0e04bb2a4
Rasmus Soome <rsoome16@gmail.com>
2017-12-06 11:54:23 -0800

    check whether ball too close

--- imageProcessor/ImageProcessor.py
+++ imageProcessor/ImageProcessor.py
@@ -35,7 +35,7 @@ class ImageProcessor:
         x, y, w, h = cv2.boundingRect(c)
 
         # If the rectangle surrounding the biggest blob is big enough, try to write it's coordinates into the object given
-        if w * h >= self.obj.minSize and self.verticalLowerBound + y + h/2 >  440:
+        if w * h >= self.obj.minSize and self.verticalLowerBound + y + h/2 <  440:
             # Check, whether another thread has signalled a cancellation of the job, and stop if the job is cancelled
             if self.cancelToken.isCanceled:
                 return
